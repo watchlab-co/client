7e337ca7a2e6e2036ad63022cb18e5eb804fe2a4
watchlab-co <officialwatchlab@gmail.com>
2025-02-24 21:15:09 +0530

    COD off

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -166,10 +166,10 @@ const PlaceOrder = () => {
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''} `}></p>
               <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
             </div>
-            <div onClick={() => setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            {/* <div onClick={() => setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''} `}></p>
               <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
-            </div>
+            </div> */}
           </div>
 
           <div className='w-full text-end mt-8'>
