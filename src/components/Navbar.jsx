8e07fe9c52a7237d68fdab834879adabf40834a2
watchlab-co <officialwatchlab@gmail.com>
2025-02-27 19:53:20 +0530

    Updated the nav link collection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,10 @@ const Navbar = () => {
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <Link to='/'><span className='text-2xl font-bold'>Watch Lab</span></Link>
-            <Link to='/collection'><span className='text-2xl '>Collections</span></Link>
+            <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+                    <p>COLLECTION</p>
+                    <hr className='w-2/4 border-npne h-[1.5px] bg-gray-700 hidden' />
+                </NavLink>
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
                 <NavLink to='/' className='flex flex-col items-center gap-1'>
