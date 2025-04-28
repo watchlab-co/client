904c2bd02133bf72a27bb727f76d72da77f17743
shaamiilll <shamilamiyan@gmail.com>
2025-04-28 23:16:32 +0530

    feat: create premium newsletter subscription component

--- src/components/NewsLetterBox.jsx
+++ src/components/NewsLetterBox.jsx
@@ -7,8 +7,8 @@ const NewsLetterBox = () => {
 
   return (
     <div className='text-center'>
-        <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
-        <p className='text-gray-400 mt-3'>Lorem ipsum is a simply text used for dummies.</p>
+        <p className='text-2xl font-medium text-gray-800'>Join Our Timekeepers Club</p>
+        <p className='text-gray-400 mt-3'>Subscribe to receive updates on new arrivals, limited editions, and special promotions.</p>
     <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none ' type="email" placeholder='Enter your email' required />
         <button className='bg-black text-white text-xs px-10 py-4' type='submit'>SUBSCRIBE</button>
